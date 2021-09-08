5c1aa938d0ac0a79efbbd9e54f1a9e9939be3c96
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-09-08 18:51:06 +0100

    adding whitespace for easy read

--- dictionary.py
+++ dictionary.py
@@ -4,15 +4,21 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(word):
+def whitespace():
+    print('\n')
+
+def define(word):
+    whitespace()
     word = word.lower()
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no:  " % get_close_matches(word, data.keys()))[0]
         if yn == "Y":
+            whitespace()
             return data[get_close_matches(word, data.keys())[0]]
         elif yn == "N":
+            whitespace()
             return "This word does not exist, please try again."
         else:
             return "Sorry, that entry is not recognised."
@@ -21,7 +27,7 @@ def translate(word):
 
 word = input("Enter a word to get the definition: ")
 
-output = translate(word)
+output = define(word)
 
 if type(output) == list:
     for item in output:
